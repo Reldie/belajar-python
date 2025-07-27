9ef877b6e72addce326624d16c5487b5054b30a0
Reldie <ahmadridho99847@gmail.com>
2025-07-27 09:46:19 +0700

    function

--- Basic.py
+++ Basic.py
@@ -53,7 +53,7 @@ else:
     print("Masukkan angka yang valid!")
 """
 
-#{} dict
+#{} dict (key : value)
 """
 siswa = {
     "nama" : "Ridho",
@@ -81,7 +81,6 @@ students = {
 print(students["001"]["nama"])
 """
 
-"""
 students = [
   {
     "nama": "Andi Saputra",
@@ -125,6 +124,7 @@ students = [
   }
 ]
 
+"""
 print(students[0]["nilai"])
 print(students)
 for i in students:
@@ -132,18 +132,46 @@ for i in students:
     if([0] ):
         print(["nama"])
 """
-        
+"""     
+for student in students:
+    if student["umur"] >= 17:
+        print(f"{student["nama"]} sudah bisa mendapatkan ktp")
+    else: 
+        print(f"{student["nama"]} belum bisa mendapatkan ktp")
+"""
+"""
+for student in students:
+    print(f"Nama : {student["nama"]}")
+    nilai_siswa = student["nilai"]
+    for mapel, nilai in nilai_siswa.items():
+        print(f"{mapel} : {nilai}")
+    rata_rata = sum(nilai_siswa.values())/len(nilai_siswa)
+    print(f"Nilai rata-rata : {rata_rata:.2f}\n")
+"""
 
+#latihan kalau nilainya lebih dari 85 lulus kalau kurang dari 85 gak lulus
+"""
+for student in students:
+    total_nilai = sum(student["nilai"].values())
+    print (f"Total nilai {student["nama"]} adalah {total_nilai}")
+    nilai_rata_rata = total_nilai / len(student["nilai"])
+    print(f"Total nilai {student['nama']} adalah {total_nilai}, dengan rata-rata {nilai_rata_rata:.2f}")
+    if nilai_rata_rata >= 85:
+        print(f"{student['nama']} lulus dengan nilai rata-rata {nilai_rata_rata:.2f}")
+    else:
+        print(f"{student['nama']} tidak lulus dengan nilai rata-rata {nilai_rata_rata:.2f}")
+"""
 #[] list 
 """
 nilai = [100, 90, 80, 79, 92]
 print(nilai)
 print(nilai[2])
-print(nilai[2][1])
+print(str(nilai[2])[1])
 print(nilai*2)
 for i in nilai:
     if(i % 2 == 1):
-        print(i)
+        print(f"{i} adalah bilangan ganjil" )
+
 
 buah = ["pisang", "jambu", "mangga", "durian"]
 print(buah)
@@ -156,6 +184,137 @@ buah.remove("jambu")
 print(buah)
 """
 
-
-
 #() tuple
+"""
+"""
+
+#input
+"""
+nama = input("masukkan nama anda:")
+alamat = input("masukkan alamat anda:")
+umur = int(input("masukkan umur anda:"))
+
+print("nama anda adalah" + nama)
+print("alamat anda adalah" + alamat)
+
+if umur >= 18:
+    print("anda seharusnya memiliki ktp")
+else:
+    print("anda seharusnya belum memiliki ktp")
+
+print(f"nama anda adalah {nama}")
+print(f"alamat anda adalah {alamat}")
+print(f"umur anda adalah {umur}")
+
+print("nama anda adalah {}".format(nama))
+print("alamat anda adalah {}".format(alamat))
+print("umur anda adalah {}".format(umur))
+"""
+
+#function
+"""
+def greet (nama):
+    nama = input("masukkan nama anda:")
+    print("hello world", nama)
+
+greet("") #memanggil fungsi greet
+""" 
+
+"""
+numbers1 = [23,45,66,76,98,100,34,56,78,90]
+numbers2 = [44,55,66,77,88,99,100,101]
+numbers3 = [12,34,56,78,90,123,456,789]
+
+def check_even_odd(numbers):
+    for  number in numbers:
+        if number % 2 == 0:
+            print(f"{number} adalah bilangan genap")
+        else: 
+            print(f"{number} adalah bilangan ganjil")
+
+check_even_odd(numbers1)
+check_even_odd(numbers2)
+check_even_odd(numbers3)
+"""
+
+#pr bikin function simulasi login, parameter username, kalau saat dipanggil usernamenya sama maka berhasil login kalau tidak maka tidak berhasil login
+"""
+def simulasi_login(username):
+    user = input("Masukkan username: ")
+    if user == username:
+        print(f"Login akun dengan username {username} berhasil! ")
+    else:
+        print("Login gagal! Username tidak sama.")
+
+simulasi_login("ridho")
+"""
+
+#nama = input("Masukkan nama anda: ")
+"""
+def ubah_vocal_ke_angka(angka):
+    hasil = "" 
+
+    for huruf in angka:
+        if huruf == "e":
+            hasil += "3"
+        elif huruf == "a":
+            hasil += "4"
+        elif huruf == "i":
+            hasil += "1"            
+        elif huruf == "u":
+            hasil += "2"
+        elif huruf == "o":
+            hasil += "0"        
+        else:
+            hasil += huruf
+    print(hasil)
+    return hasil
+
+ubah_vocal_ke_angka("tikus")
+"""
+
+"""
+def naik_satu_tingkat(letters):
+    
+    result = ""
+    for letter in letters:
+        if letter not in ["a", "e", "i", "o", "u", "3", "4", "1", "2", "0"]:   
+            kode = ord(letter)
+            if letter >= "a" and letter <= "z":
+                if letter == "z":
+                    kode_baru = ord("a")
+                else:
+                    kode_baru = kode + 1
+            else: 
+                result += letter
+        else: 
+            result += letter
+    return result
+
+def final(nama):
+    langkah1 = ubah_vocal_ke_angka(nama)
+    langkah2 = naik_satu_tingkat(langkah1)
+
+print(final("Setiawan"))
+"""
+
+#pr ubah huruf besar jadi angka 
+"""
+def ubah_huruf_besar_ke_angka(teks):
+    hasil = ""
+
+    for huruf in teks:
+        if huruf == "A":
+            hasil += "1"
+        elif huruf == "B":
+            hasil += "2"
+        elif huruf == "C":
+            hasil += "3"
+        elif huruf == "D":
+            hasil += "4"
+        else:
+            hasil += huruf
+    print(hasil)
+
+ubah_huruf_besar_ke_angka("ayAm")
+"""
